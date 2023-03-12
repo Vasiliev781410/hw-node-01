cfbdc60c867a273fb6bbf3d1815c0e49a782c3b8
Vasiliev781410 <37668858+Vasiliev781410@users.noreply.github.com>
2023-03-12 17:21:46 +0200

    1

---
+++ contacts.js
@@ -0,0 +1,25 @@
+const fs = require('fs').promises;
+const path = require('path');
+const contactsPath = path.join(__dirname,'/contacts.json');
+
+
+ console.log(contactsPath);
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+    // ...твой код
+  }
+  
+  function getContactById(contactId) {
+    // ...твой код
+  }
+  
+  function removeContact(contactId) {
+    // ...твой код
+  }
+  
+  function addContact(name, email, phone) {
+    // ...твой код
+  }
+
+  module.exports = {listContacts, getContactById, removeContact, addContact};
